e820f179f64f902d87a23107aadb81fce8702279
Jérémie Meyer <jm@sukoa.com>
2021-04-18 14:43:49 +0200

    db + accueil

--- config/database.js
+++ config/database.js
@@ -1,15 +1,38 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+module.exports = ({ env }) => {
+  if (!env('DEV_ENV', false)) {
+    return ({
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'bookshelf',
+          settings: {
+            client: 'mysql',
+            host: env('DATABASE_HOST'),
+            port: env.int('DATABASE_PORT'),
+            database: env('DATABASE_NAME'),
+            username: env('DATABASE_USERNAME'),
+            password: env('DATABASE_PASSWORD'),
+            ssl: env.bool('DATABASE_SSL', false),
+          },
+          options: {}
+        },
       },
-      options: {
-        useNullAsDefault: true,
+    });
+  } else {
+    return ({
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'bookshelf',
+          settings: {
+            client: 'sqlite',
+            filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+          },
+          options: {
+            useNullAsDefault: true,
+          },
+        },
       },
-    },
-  },
-});
+    });
+  }
+}
